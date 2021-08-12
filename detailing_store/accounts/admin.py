398e1b738f5f52ccb311c8742e5a4e81f764033b
Norbert M <79285346+Nor-Mal@users.noreply.github.com>
2021-08-12 10:10:47 +0100

    Delete detailing_store/accounts directory

--- detailing_store/accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Customer, Address
-
-# Register your models here.
-admin.site.register(Customer)
-admin.site.register(Address)
-# admin.site.unregister(auth.models.User)
-# admin.site.unregister(auth.models.Group)
