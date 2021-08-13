6b824e87bbeddd9c8a9e0f27bd082a52fe1a437f
Norbert M <79285346+Nor-Mal@users.noreply.github.com>
2021-08-13 12:21:37 +0100

    Update paypal.py

--- shopping_basket/paypal.py
+++ shopping_basket/paypal.py
@@ -3,7 +3,7 @@ from paypalcheckoutsdk.core import PayPalHttpClient, SandboxEnvironment
 
 class PayPalClient:
     def __init__(self):
-        self.client_id = "AcftcPBhlvoYgaCYHZvJjJ2BtInnUAdFbeqsL9i54_1vuzw4NfscVdIh9YvGwQ7iu-tRSjyIvcL5SQLz"
-        self.client_secret = "EDEf1Lp4LHQWfS6EXrnWOBGYShsD8ESUUr2Km086IB9lbFcewNhfe_lCi0Am1xT5uVqHHFe6z0ZuNe0V"
+        self.client_id = "Replace this text with your PayPal sandbox business CLIENT ID"
+        self.client_secret = "Replace this text with your PayPal sandbox business CLIENT SECRET"
         self.environment = SandboxEnvironment(client_id=self.client_id, client_secret=self.client_secret)
         self.client = PayPalHttpClient(self.environment)
